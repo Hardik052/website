c5ef99136eb155962e7b2831504dd8e052114897
Hardik052 <bhardwajhardik012@gmail.com>
2023-08-10 14:00:41 -0500

    Validations working!

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,5 @@ class Category < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "id", "name", "updated_at"]
   end
+  validates :name, presence: true
 end
--- app/models/tax_rate.rb
+++
@@ -1,2 +0,0 @@
-class TaxRate < ApplicationRecord
-end
