f10f02f8e1f1fca5497dbdc468c8b0c7c606e532
Hardik052 <bhardwajhardik012@gmail.com>
2023-08-10 12:54:06 -0500

    adding in active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 
 
   post 'update_cart_address', to: 'carts#update_cart_address', as: :update_cart_address
